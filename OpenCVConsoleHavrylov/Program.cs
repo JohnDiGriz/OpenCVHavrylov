1e72de513ca0b86a69baa72e1397eec6bd2a3ebf
Serhii Havrylov <shavrylov@tennis-point.de>
2021-05-10 18:46:02 +0300

    Removed QR-Code request

--- OpenCVConsoleHavrylov/Program.cs
+++ OpenCVConsoleHavrylov/Program.cs
@@ -13,19 +13,10 @@ namespace OpenCVConsoleHavrylov
             {
                 Console.WriteLine("Enter Image Path: ");
                 var path = Console.ReadLine();
-                Console.WriteLine("Press q to detect QrCode, or f to detect face");
-                var key = Console.ReadKey();
                 Console.WriteLine();
                 try
                 {
-                    if (key.Key == ConsoleKey.Q)
-                    {
-                        Console.WriteLine($"QR Data: {OpenCVWrapper.DecodeQR(path)}");
-                    }
-                    else if(key.Key == ConsoleKey.F)
-                    {
-                        Console.WriteLine($"Found faces: {OpenCVWrapper.RecognizeFaces(path)}");
-                    }
+                    Console.WriteLine($"Found faces: {OpenCVWrapper.RecognizeFaces(path)}");
                     var result = path == null ? $"{Directory.GetCurrentDirectory()}/result.png" : $"{path}result.{path.Split('.').Last()}";
                     Console.WriteLine($"Resulting image saved as {result}");
                     Console.WriteLine("Press any key");
